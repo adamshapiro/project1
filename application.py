37b72fdd3a1ef5a8598944defd8fc4d6b3799294
adamshapiro <adam.shapiro595@gmail.com>
2018-07-04 13:32:43 -0400

    added routing for logging in and viewing location info

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +24,24 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if not session["logged_in"]:
+        return redirect(url_for( 'login' ))
+    return render_template("index.html")
+
+@app.route("/login")
+def login(new_user=False):
+    return render_template("login.html", new_user=new_user)
+
+@app.route("/sign_in", methods=["POST"])
+def sign_in():
+    user = request.form
+    # TODO: USER AUTHENTICATION
+    return redirect(url_for("index"))
+
+@app.route("/location")
+def location():
+    zip = request.args.get("zip")
+    # TODO: query database
+    query = requests.get(f"https://api.darksky.net/forecast/644dbdc3d30b81ecd8f71b0da4d17d09/{lat},{long}").json()
+    weather = query["current"]
+    return render_template("location.html", location=loc, weather=weather)
