8f36a37ec7c47d90c27f91cd59aa51954b610999
adamshapiro <adam.shapiro595@gmail.com>
2018-07-12 11:30:05 -0400

    added comments and finished the README file

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # Project 1
 
-Web Programming with Python and JavaScript
+### Web Programming with Python and JavaScript
+
+This Website allows visitors to log in or sign up accounts so that they can view
+detailed information for various locations throughout the US, as well as check in
+to each location with a personalized message.
+The following files are used:
+
+### HTML
+---
+* [layout.html](/templates/layout.html): The basic layout extended by each other html page.
+    Includes a navbar for logging out and returning to the search page.
+
+* [index.html](/templates/index.html): The search page. Users can search by zip code, city, or state.
+    The results will then be displayed in a table below the search form, with an option
+    to view more details for any location.
+
+* [location.html](/templates/location.html): A page used for displaying more details about
+    a given location. These details include latitude and longitude, population, as well
+    as current weather information taken from the DarkSky API. This page also allows a user
+    to check in to the location with an optional message, and shows the number of check ins
+    and messages already left.
+
+* [login.html](/templates/login.html): The login and sign up page. The same page is used
+    for both login and signup, and users can alternate between the two. Users must be logged in
+    before visiting the search or location pages.
+
+### Python
+---
+* [application.py](/application.py): The main file for running the flask server. Controls
+    routing for each HTMl page, as well as an API route for getting a small amount of information
+    for a location via its zip code.
+
+* [models.py](/models.py): A file containing ORM information for the database tables:
+    * **Location**
+    * **User**
+    * **Check_In** (used for mapping a location id and a user id to a message)
+
+* [import.py](/import.py): A file for importing a csv of location information to
+    to the appropriate database table.
+
+### MISC
+---
+* [requirements.txt](/requirements.txt): A list of python modules that must be installed before
+    running the application.
+
+* [zips.csv](/zips.csv): The CSV file of locations imported into the database
--- application.py
+++ application.py
@@ -24,17 +24,22 @@ db.init_app(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """Search locations"""
+    # users must be logged in before using the site
     if not "user_id" in session:
         return redirect(url_for( 'login' ))
 
     locs = []
 
+    # search the locations database
     if request.method == "POST":
         method = request.form["method"]
         search = request.form['search'].upper()
+        # change the column to search based on the form dropdown
+        # limit to 40 responses to avoid excessive response
         locs = Location.query.filter(
                 getattr(Location, method).like(f"%{search}%")
-            ).limit(20).all()
+            ).limit(40).all()
         if len(locs) == 0:
             flash("We couldn't find any locations matching that information!", "danger")
 
@@ -42,21 +47,26 @@ def index():
 
 @app.route("/login")
 def login():
+    """Log in or sign up"""
+    # use the same template for login and signup
     new_user = request.args.get("new_user", False)
     return render_template("login.html", new_user=new_user)
 
 @app.route("/logout")
 def logout():
+    """Log out"""
     session.clear()
     return redirect(url_for('login'))
 
 @app.route("/sign_in", methods=["POST"])
 def sign_in():
+    """Sign into your account"""
     username = request.form["username"]
+    # password must be encoded into bytes for bcrypt to work
     password = request.form["password"].encode()
 
     db_user = User.query.filter_by(username=username).first()
-
+    # show an error if the user doesnt exist or the password is wrong
     if not db_user or not bcrypt.checkpw(password, db_user.password.encode()):
         flash("Your username and/or password is incorrect!", "danger")
         return render_template("login.html")
@@ -67,10 +77,12 @@ def sign_in():
 
 @app.route("/sign_up", methods=["POST"])
 def sign_up():
+    """Sign up for a new account"""
     username = request.form["username"]
     password = request.form["password"].encode()
     confirm = request.form["confirm"].encode()
 
+    # username and password cant be blank
     if len(username) == 0 or len(password) == 0:
         flash("You must enter a username and password.", "danger")
         return render_template("login.html", new_user=True)
@@ -79,40 +91,52 @@ def sign_up():
         flash("Your password and confirmation do not match!", "danger")
         return render_template("login.html", new_user=True)
 
+    # username cannot be the same as another users username
     if User.query.filter_by(username=username).first():
         flash("That username is taken!", "danger")
         return render_template("login.html", new_user=True)
 
+    # create a password hash to store instead of plaintext
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    # hashed is generate as a bytearray, so it must be decoded into a string before storing
     res = User(username=username, password=hashed.decode())
     db.session.add(res)
     db.session.commit()
 
+    # add the user id and username to session, and route to index
     session["user_id"] = res.id
     session["username"] = username
     return redirect(url_for("index"))
 
 @app.route("/<int:id>")
 def location(id):
+    """Get more information on a specific location"""
+    # user must be logged in before accessing
     if not "user_id" in session:
         return redirect(url_for( 'login' ))
 
     loc = Location.query.get(id)
 
+    # make sure the id matches an actual location in the database
     if loc is None:
         flash("No such location could not be found.", "danger")
         return redirect(url_for('index'))
 
+    # search the DarkSky Api for weather information
     query = requests.\
         get(f"https://api.darksky.net/forecast/644dbdc3d30b81ecd8f71b0da4d17d09/{loc.latitude},{loc.longitude}").json()
 
+    # we only need current weather information
     weather = query["currently"]
+    # time is sent as a Unix timestamp, so convert to a proper date and time string
     time = datetime.fromtimestamp(weather["time"]).strftime("%Y-%m-%d %I:%M %p")
     return render_template("location.html", location=loc, weather=weather, time=time)
 
 @app.route("/check_in", methods=["POST"])
 def check_in():
+    """Check into a location"""
     check = request.form
+    # make sure the currently logged in user has not already checked into the location
     if Check_In.query.filter(and_(
         Check_In.location_id == check["id"],
         Check_In.user_id == session["user_id"]
@@ -120,6 +144,7 @@ def check_in():
         flash("You have already checked into this location!", "danger")
         return redirect(url_for('location', id=check["id"]))
 
+    # create a new check in and add it to the database
     new_check = Check_In(
         location_id=check["id"],
         user_id=session["user_id"],
@@ -132,7 +157,9 @@ def check_in():
 
 @app.route("/api/<string:zip>")
 def zip(zip):
+    """API for getting info of a zip code"""
     query = Location.query.filter_by(zip=zip).first()
+    # make sure zip code exists in database
     if query is None:
         return abort(404)
 
